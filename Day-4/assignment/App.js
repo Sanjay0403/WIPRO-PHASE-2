4ac099740e754048bd138bf59f6c79e388ccbada
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-03-04 16:19:41 +0530

    Add files via upload

---
+++ Day-4/assignment/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeeForm from './EmployeeForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeForm />
+    </div>
+  );
+}
+
+export default App;
