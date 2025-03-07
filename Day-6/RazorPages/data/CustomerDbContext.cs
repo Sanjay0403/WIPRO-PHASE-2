135fa3b10b8882fc3df9858fea428b12527d016f
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-03-07 14:25:33 +0530

    Add files via upload

---
+++ Day-6/RazorPages/data/CustomerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define a DbSet for Customer entities
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
