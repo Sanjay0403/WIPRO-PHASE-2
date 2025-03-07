04d87872e1f5df0539fb20a35be41819b9579447
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-03-07 14:26:27 +0530

    Add files via upload

---
+++ Day-6/RazorPages/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Phoneno { get; set; }
+
+        // Add DateCreated field to store the creation date
+        public DateTime DateCreated { get; set; }
+    }
+}
