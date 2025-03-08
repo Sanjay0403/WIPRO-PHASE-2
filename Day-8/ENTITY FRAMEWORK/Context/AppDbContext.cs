0f2b68b0472ccb596a948e77a6e98eae4fc01e67
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-03-08 16:35:39 +0530

    ENTITY FRAMEWORK

---
+++ Day-8/ENTITY FRAMEWORK/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_EFCore.Models;
+
+namespace MVC_EFCore.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Employee>  Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=ANAMIKA\\SQLSERVER;database=DemoDb2;integrated security=true;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ Day-8/ENTITY FRAMEWORK/Controllers/EmployeeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_EFCore.Context;
+using MVC_EFCore.Models;
+
+namespace MVC_EFCore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //AppDbContext _context = new AppDbContext();
+        AppDbContext _context;
+        public EmployeeController(AppDbContext context)
+        {
+
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            return View(_context.Employees.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            return View(employee);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Employee employee)
+        {
+            var obj = _context.Employees.FirstOrDefault(x => x.Id == id);
+            obj.Dept = employee.Dept;
+            obj.Salary = employee.Salary;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            return View(employee);
+        }
+
+        public IActionResult Deleted(int id, Employee employee)
+        {
+            var obj = _context.Employees.FirstOrDefault(x => x.Id == id);
+            _context.Employees.Remove(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
