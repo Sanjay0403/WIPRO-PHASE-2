5cf9f7ee6b4d84ff60080460616be7e7014ac86a
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-03-08 16:32:03 +0530

    MVC

---
+++ Day-8/MVC/Controllers/StudentController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_DemoDay8.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVC_DemoDay8.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<Student> students = null;
+
+        public StudentController()
+        {
+            if (students == null)
+            {
+                students = new List<Student>()
+                {
+                    new Student() { Id = 1, Name = "ajay", Batch = "B001", Marks = 90 },
+                    new Student() { Id = 2, Name = "jay", Batch = "B002", Marks = 90 },
+                    new Student() { Id = 3, Name = "vijay", Batch = "B002", Marks = 90 },
+                    new Student() { Id = 4, Name = "deepak", Batch = "B003", Marks = 90 }
+                };
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var student = students.FirstOrDefault(x => x.Id == id);
+            return View(student);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = students.FirstOrDefault(x => x.Id == id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Student student)
+        {
+            var obj = students.FirstOrDefault(x => x.Id == id);
+            if (obj != null)
+            {
+                obj.Batch = student.Batch;
+                obj.Marks = student.Marks;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = students.FirstOrDefault(x => x.Id == id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var obj = students.FirstOrDefault(x => x.Id == id);
+            if (obj != null)
+            {
+                students.Remove(obj);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Day-8/MVC/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_DemoDay8.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Batch { get; set; }
+        public int Marks { get; set; }
+    }
+}
